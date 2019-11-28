f9c3a63968fe0fb275baf9892b917d0636de00e5
Elmon Noronha <elmon.noronha@gmail.com>
2019-11-28 14:02:16 -0300

    Clean Repo

---
+++ src/desafiocljapi/db/core.clj
@@ -0,0 +1,2 @@
+(ns desafiocljapi.db.core)
+
---
+++ src/desafiocljapi/db/queries.clj
@@ -0,0 +1,39 @@
+(ns desafiocljapi.db.queries
+  (:require [desafiocljapi.db.core]
+            ;[korma.core :refer :all]
+            )
+  )
+
+(defentity items)
+
+(defn get-todos []
+  (select items)
+  )
+
+(defn add-todo [title description]
+  (insert items
+          (values {:title title :description description})
+          )
+  )
+
+(defn delete-todo [id]
+  (delete items
+          (where {:id [= id]})
+          )
+  )
+
+(defn update-todo [id title is-complete]
+  (update items
+          (set-fields {:title title
+                       :is_complete is-complete})
+          (where {:id [= id]})
+          )
+  )
+
+(defn get-todo [id]
+  (first
+    (select items
+          (where {:id [= id]})
+          )
+    )
+  )
